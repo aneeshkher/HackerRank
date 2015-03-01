bf195fe1346b147c1fd9f3524aa89a21edfb973d
aneeshkher <aneeshkher@gmail.com>
2015-02-28 21:25:49 -0500

    Creating folder and adding README

---
+++ Filling-Jars/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inputLine = sc.nextLine();
+
+		String[] tokens = inputLine.split("\\s+");
+		int jars = Integer.parseInt(tokens[0]);
+		int operations = Integer.parseInt(tokens[1]);
+
+		
+
+	}
+}
